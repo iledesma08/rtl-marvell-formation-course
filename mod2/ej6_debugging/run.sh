f3fc49e6d4b54e11dd9ef326fa996c9b43920f57
JRondon23 <166946314+JRondon23@users.noreply.github.com>
2026-08-06 00:10:11 -0300

    Readme de ejercicio 6 y cambio de nombre del Readme del ejercicio 3

--- mod2/ej6_debugging/run.sh
+++ mod2/ej6_debugging/run.sh
@@ -7,7 +7,7 @@ echo ">>> Compilando con iverilog (SystemVerilog 2012)..."
 iverilog -g2012 -o ejercicio6.out alu_bad.v alu_fix1.v alu_fix2.v tb_alu.v
  
 echo ">>> Ejecutando con vvp..."
-vvp "ejercicio6.out"
+vvp "ejercicio6.out" | tee resultados.txt
  
 echo "ejercicio6.out ejecutado"
 echo "VCD generado: tb_alu.vcd (abrir con: gtkwave tb_alu.vcd)"
